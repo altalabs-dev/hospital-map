ff3fdd5b513b01e0198fa60562d7fbc700e225ee
Công <cong.pc2305@gmail.com>
2023-09-07 20:47:04 +0700

    fix

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import hospitals from "./hospital.json" assert { type: "json" };
-
 var map;
 var geocoder = new google.maps.Geocoder();
 var infoWindow = new google.maps.InfoWindow({
@@ -18,8 +16,9 @@ const postcode_el = document.getElementById("homepoint");
 const icon_marker =
   '<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 10C19 13.9765 12 21 12 21C12 21 5 13.9765 5 10C5 6.02355 8.13401 3 12 3C15.866 3 19 6.02355 19 10Z" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/><circle cx="12" cy="10" r="3" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/></svg>';
 const title = document.getElementById("title");
-let data = hospitals;
+let data = [];
 async function init() {
+  data = await fetch("./hospital.json").then((res) => res.json());
   data = data.filter((item) => item.Latitude && item.Longtitude);
   var mapOptions = {
     zoom: zoom,
